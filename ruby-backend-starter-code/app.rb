cf10d463d97d3a3fcacaf1a8fdfc29a83e2af7c5
Spicy Legato <jpqreichard@gmail.com>
2016-05-28 16:50:18 -0400

    closes unclosed methods & conditionals, File method in route view, adds params to favorites route

--- ruby-backend-starter-code/app.rb
+++ ruby-backend-starter-code/app.rb
@@ -1,18 +1,19 @@
 require 'sinatra'
 
-get '/'
-  File.read('index.html')
+get '/' do
+  File.open('views/index.html')
 end
 
-get 'favorites' do
+get '/favorites' do
   response.header['Content-Type'] = 'application/json'
   File.read('data.json')
 end
 
-get '/favorites' do
+get '/favorites/:name/:oid' do
   file = JSON.parse(File.read('data.json'))
-  unless params[:name] && params[:oid]
+  unless params[:name] && params[:oid] then
     return 'Invalid Request'
+  end
   movie = { name: params[:name], oid: params[:oid] }
   file << movie
   File.write('data.json',JSON.pretty_generate(file))
